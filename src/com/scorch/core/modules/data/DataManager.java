100494ea4158b07465c0d0a82f93a5a5dabc5faf
MSWS <imodmaker@gmail.com>
2019-06-22 21:49:47 -0700

    I know git I swear (readded Gijs' commits)

--- src/com/scorch/core/modules/data/DataManager.java
+++ src/com/scorch/core/modules/data/DataManager.java
@@ -12,16 +12,15 @@ import org.bukkit.Location;
 import com.google.gson.Gson;
 import com.scorch.annotations.DataIgnore;
 import com.scorch.annotations.DataNotNull;
-import com.scorch.core.ScorchCore;
 import com.scorch.core.modules.AbstractModule;
 import com.scorch.utils.Logger;
 
-public class DataManager extends AbstractModule {
+public class DataManager extends AbstractModule{
 
 	private static Gson gson = new Gson();
-
+	
 	private ConnectionManager connectionManager;
-
+	
 	public DataManager(String id, ConnectionManager connectionManager) {
 		super(id);
 		this.connectionManager = connectionManager;
@@ -30,74 +29,26 @@ public class DataManager extends AbstractModule {
 	@Override
 	public void initialize() {
 		// TODO Auto-generated method stub
-		// Open connection probably here
+		
 	}
 
 	@Override
 	public void disable() {
 		// TODO Auto-generated method stub
+		
 	}
-
-	public void saveData() {
-
-	}
-
-	// TODO
-	public void setData(Object key, Object value) {
-	}
-
-	// TODO
-	public void configSet(String key, Object value) {
-		ScorchCore.getInstance().getConfig().set(key, value);
-	}
-
+	
 	/**
-<<<<<<< HEAD
-	 * Creates a table with <code>name</code> as name and with the variables of
-	 * <code>storageType</code> as columns
-	 * 
-=======
 	 * Creates a table if it doesn't exist already 
 	 * with <code>name</code> as name and with the 
 	 * variables of <code>storageType</code> as columns
 	 * Call this in your plugin/module initialisation
->>>>>>> 6888e607ca65522422a0abe1719f2666d7a04799
 	 * @param name        the name of the database
 	 * @param storageType the column template
 	 */
-	public void createTable(String name, Object storageType) {
-
+	public void createTable (String name, Object storageType) {
+		
 		String query = "CREATE TABLE IF NOT EXISTS " + name + " (table_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, ";
-<<<<<<< HEAD
-
-		for (int i = 0; i < storageType.getClass().getFields().length; i++) {
-
-			Field field = storageType.getClass().getFields()[i];
-
-			// Makes sure that the field doesnt have to be ignored for serialisation
-			if (field.getAnnotation(DataIgnore.class) == null) {
-
-				query += field.getName();
-
-				if (field.getType() == Integer.class) {
-					query += " INT ";
-				} else if (field.getType() == String.class) {
-					query += " TEXT ";
-				} else if (field.getType() == Long.class) {
-					query += " BIGINT ";
-				} else if (field.getType() == UUID.class) {
-					query += " VARCHAR(36) ";
-				} else if (field.getType().isEnum()) {
-					query += " TEXT ";
-				} else if (Collection.class.isAssignableFrom(field.getClass())) {
-					query += " TEXT ";
-				} else if (Map.class.isAssignableFrom(field.getClass())) {
-					query += " TEXT ";
-				} else if (field.getType() == Location.class) {
-					query += " TEXT ";
-				} else {
-					query += " TEXT ";
-=======
 		
 		for(int i = 0; i < storageType.getClass().getFields().length; i++) {
 			Field field = storageType.getClass().getFields()[i];
@@ -140,22 +91,10 @@ public class DataManager extends AbstractModule {
 				}
 				else if(i != storageType.getClass().getFields().length) {
 					query = query + ", ";
->>>>>>> 6888e607ca65522422a0abe1719f2666d7a04799
 				}
-
-				if (field.getAnnotation(DataNotNull.class) != null) {
-					query += "NOT NULL, ";
-				} else if (i != storageType.getClass().getFields().length) {
-					query += ", ";
-				} else {
-					query += ");";
+				else {
+					query = query + ");";
 				}
-<<<<<<< HEAD
-			}
-		}
-	}
-
-=======
 				
 				
 				this.getConnectionManager().executeQuery(query);
@@ -244,22 +183,19 @@ public class DataManager extends AbstractModule {
 		
 	}
 	
->>>>>>> 6888e607ca65522422a0abe1719f2666d7a04799
 	/**
 	 * Gets the gson instance
-	 * 
 	 * @return the gson instance
 	 */
-	public static Gson getGson() {
+	public static Gson getGson () {
 		return gson;
 	}
-
+	
 	/**
 	 * Gets the connectionmanager
-	 * 
 	 * @return the connection manager
 	 */
-	private ConnectionManager getConnectionManager() {
+	private ConnectionManager getConnectionManager () {
 		return this.connectionManager;
 	}
 }
